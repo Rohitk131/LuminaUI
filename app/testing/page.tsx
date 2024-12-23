8f8ba5fc0f78ab40439ecd751820b64f77754c1d
MurtazaKhannn <murtazakhan1910@gmail.com>
2024-12-23 18:36:40 +0530

    flying button component

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,8 +1,22 @@
+'use client'
+
 import React from 'react'
+
+import FlyingButton from '../(docs)/docs/flyingbutton/flying-button';
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Testing Flying Button</h1>
+      <FlyingButton
+        name="Button"
+        color="#ff0000"
+        video="/assets/fbutton.mp4"
+        link="https://Murtazadev-one.vercel.app"
+        icon={<div>🔥</div>}
+      />
+    </div>
   )
 }
 
