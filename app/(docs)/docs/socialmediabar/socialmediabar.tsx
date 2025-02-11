430a1dfa83699f6718b665ca3deeb21d812d25cf
MurtazaKhannn <murtazakhan1910@gmail.com>
2025-02-12 02:31:36 +0530

    socialmedia bar preview

--- app/(docs)/docs/socialmediabar/socialmediabar.tsx
+++ app/(docs)/docs/socialmediabar/socialmediabar.tsx
@@ -1,7 +1,73 @@
+'use client'
 
 import SocialMediaBar from '@/components/luminaui/socialmediabar';
+import { useEffect, useState } from 'react';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { MdHomeMax } from 'react-icons/md';
+import { SiGithub, SiLeetcode } from 'react-icons/si';
+import { WiMoonAltWaxingCrescent2 } from 'react-icons/wi';
 
 export default function SocialMedia() {
 
-  return <SocialMediaBar />;
+  const [theme, setTheme] = useState('light');
+
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      setTheme('light');
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  };
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
+      if (storedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+      }
+    }
+  }, []);
+
+  const iconData = [
+        {
+          icon: <FaLinkedinIn />,
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/murtaza-khan-58a44a25a/",
+        },
+        {
+          icon: <SiGithub />,
+          name: "GitHub",
+          url: "https://github.com/MurtazaKhannn",
+        },
+        {
+          icon: <FaXTwitter />,
+          name: "Twitter",
+          url: "https://x.com/murtazakhan1910?t=RX_qTiCkEGTHTwNGziAgow&s=09",
+        },
+        {
+          icon: <SiLeetcode />,
+          name: "Leetcode",
+          url: "https://leetcode.com/",
+        },
+        {
+          icon: <MdHomeMax />,
+          name: "Home",
+          url: "/",
+        },
+        {
+          icon: <WiMoonAltWaxingCrescent2 />,
+          name: "Theme",
+          url: "#",
+          onClick: toggleTheme,
+        },
+      ];
+
+  return <SocialMediaBar iconData={iconData} />;
 }
