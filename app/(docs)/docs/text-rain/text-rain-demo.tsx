e1c8feddca41b4518d388c0900864c995659f967
AryanPahuja21 <aryanpahuja1421@gmail.com>
2025-02-15 01:32:36 +0530

    refactored text rain

---
+++ app/(docs)/docs/text-rain/text-rain-demo.tsx
@@ -0,0 +1,14 @@
+import TextRain from "@/components/luminaui/text-rain";
+
+const TextRainPreview = () => {
+  return (
+    <div>
+      <TextRain
+        heading="Welcome to the Future"
+        subheading="Where Innovation Meets Design"
+      />
+    </div>
+  );
+};
+
+export default TextRainPreview;
