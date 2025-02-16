58ec6d45338a4d13744c6c775d85a0cee7902c9e
Rohitk131 <rohitkumar13103@gmail.com>
2025-02-16 22:50:15 +0530

    fixing z index of orbs

--- app/(docs)/docs/components/preview-code-card.tsx
+++ app/(docs)/docs/components/preview-code-card.tsx
@@ -23,8 +23,8 @@ const PreviewCodeCard = async ({
 
   return (
     <CodeCard code={demoCode} className={cn("mb-14 mt-5", className)}>
-      <div className="absolute hidden sm:block sm:top-10 sm:left-10 md:top-[100px] md:left-[600px] w-96 h-96 bg-purple-400/20 dark:bg-purple-600/10 rounded-full blur-3xl" />
-      <div className="absolute bottom-[150px] right-[200px] w-96 h-96 bg-blue-400/20 dark:bg-blue-600/10 rounded-full blur-3xl" />
+      <div className="absolute hidden sm:block sm:top-10 sm:left-10 md:top-[100px] md:left-[600px] w-96 h-96 bg-purple-400/20 dark:bg-purple-600/10 rounded-full blur-3xl -z-50" />
+      <div className="absolute bottom-[150px] right-[200px] w-96 h-96 bg-blue-400/20 dark:bg-blue-600/10 rounded-full blur-3xl -z-50" />
       <div className="flex items-center justify-center ">{children}</div>
     </CodeCard>
   );
